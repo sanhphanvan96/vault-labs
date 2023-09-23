fefad93c823ecaa3022f7ded24326be19c4a6389
Sanh Phan Van <sphan@smacpro16.local>
2023-09-23 15:17:28 +0700

    python app

---
+++ app/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import os
+import sys
+
+app = Flask(__name__)
+
+def get_secret_from_env(ENV_VARIABLE):
+    return os.environ.get(ENV_VARIABLE)
+
+def get_secret_from_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            secrets = file.read().strip()
+            return secrets
+    except FileNotFoundError:
+        return None
+
+@app.route('/', methods=['GET'])
+def get_secret():
+    secret_from_env = get_secret_from_env('APP_TOKEN')
+    secret_from_file = get_secret_from_file('/etc/app/app.conf')
+    
+    if secret_from_env:
+        secret = secret_from_env
+    elif secret_from_file:
+        secret = secret_from_file
+    else:
+        secret = "No secret found"
+    
+    return jsonify({'secret': secret})
+
+if __name__ == "__main__":
+    app.run()
