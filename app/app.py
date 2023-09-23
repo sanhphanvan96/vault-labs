028462259b0df03bf82bf20cc7b7113d091c1d42
sphan <sphan@example.com>
2023-09-24 01:36:09 +0700

    format indent

--- app/app.py
+++ app/app.py
@@ -22,14 +22,14 @@ def get_secret_from_file(file_path):
 def get_secret():
     secret_from_env = get_secret_from_env('APP_TOKEN')
     secret_from_file = get_secret_from_file('/etc/app/app.conf')
-    
+
     if secret_from_env:
         secret = secret_from_env
     elif secret_from_file:
         secret = secret_from_file
     else:
         secret = "No secret found"
-    
+
     return jsonify({'secret': secret})
 
 if __name__ == "__main__":
